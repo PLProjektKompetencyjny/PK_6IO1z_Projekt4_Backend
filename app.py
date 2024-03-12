32f5dfff0e5ac6fda522c74947dcb03eeb4ceb1e
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-03-12 22:38:18 +0100

    add logger configuration and update requirements

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
+import logger
 from flask import Flask
+
+logger.configure_logging()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    app.logger.info("Hello, World!")
     return 'Hello, World!'
+
+
+@app.route('/error')
+def error():
+    app.logger.error("I'm an error")
+    return "I'm an error"
+
+
+@app.after_request
+def after_request(response):
+    app.logger.info(f"Request status: {response.status_code}")
+    return response
 
 
 if __name__ == '__main__':
