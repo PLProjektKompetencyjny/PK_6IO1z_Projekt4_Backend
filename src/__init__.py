6fc2e7e156e37ff4b7efad6fa63d4cf04e3d45e0
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-03-19 15:44:37 +0100

    add src folders structure

---
+++ src/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+from .config import config_by_name, LOGGING_CONFIG
+
+from logging.config import dictConfig
+
+db = SQLAlchemy()
+flask_bcrypt = Bcrypt()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    dictConfig(LOGGING_CONFIG)
+    app.config.from_object(config_by_name[config_name])
+    db.init_app(app)
+    flask_bcrypt.init_app(app)
+
+    return app
---
+++ src/config.py
@@ -0,0 +1,63 @@
+from os import getenv
+
+postgres_local_base = 'postgresql://baeldung:baeldung@localhost/postgres'
+
+
+class Config:
+    SECRET_KEY = getenv('SECRET_KEY', 'my_precious_secret_key')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
+
+LOGGING_CONFIG = {
+    'version': 1,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] [%(module)s] - [%(levelname)s] : %(message)s",
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+        'file': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': 'logs/app.log',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 5,
+            'formatter': 'default'
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['console', 'file']
+    },
+}
