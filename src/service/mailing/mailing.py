8100a8e461194fbd827fb41eb2d313d14c746dc4
rukaSUKII <sobonukasz@gmail.com>
2024-03-26 16:50:03 +0100

    Add sending email class, abstract class for message creators, implemented creators without connection to DB

--- src/service/mailing/mailing.py
+++ src/service/mailing/mailing.py
@@ -1,50 +1,119 @@
-from flask import Flask, redirect, url_for, session
-from oauth2client.service_account import ServiceAccountCredentials
-import json
+import smtplib
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+from os import getenv
+from abc import ABC, abstractmethod
 
-app = Flask(__name__)
-app.secret_key = 'super secret key'
+load_dotenv()
+EMAIL = getenv('EMAIL_ADDRESS')
+PASSWORD = getenv('EMAIL_PASS')
+LOGO_PATH = getenv('LOGO_HTTPS_PATH')
+
+class CreateMail(ABC):
+    def __init__(self, dataID):
+        self.message = None
+        self.recipients = ['sobonukasz@gmail.com']
+        self.dataID = dataID
+        self.logo_path = LOGO_PATH
+
+    @abstractmethod
+    def create_message(self):
+        pass
+
+class CreateResetPasswordMessage(CreateMail):
+    def __init__(self, dataID):
+        super().__init__(dataID)
+        self.html_path = '../../templates/HTML_EMAIL/password.html'
+
+    def create_message(self):
+        html_message = open(self.html_path).read()
+        html_message = html_message.replace('{logo_path}', self.logo_path).replace('{reset_password}', 'onet.pl')
+
+        self.message = MIMEText(html_message, 'html')
+        self.message['Subject'] = 'Reset password'
+        self.message['From'] = EMAIL
+        self.message['To'] = ', '.join(self.recipients)
+        return self.message
+
+class CreateReservationConfirmationMessage(CreateMail):
+    def __init__(self, dataID):
+        super().__init__(dataID)
+        self.html_path = '../../templates/HTML_EMAIL/reservation-confirmation.html'
+
+    def create_message(self):
+        html_message = open(self.html_path).read()
+        html_message = html_message.replace('{logo_path}', self.logo_path).replace('{reservation_id}', str(self.dataID))
+
+        self.message = MIMEText(html_message, 'html')
+        self.message['Subject'] = f'Reservation with ID {self.dataID} confirmed!'
+        self.message['From'] = EMAIL
+        self.message['To'] = ', '.join(self.recipients)
+        return self.message
+
+class CreateInvoiceMessage(CreateMail):
+    def __init__(self, dataID):
+        super().__init__(dataID)
+        self.html_path = '../../templates/HTML_EMAIL/invoice.html'
+
+    def create_message(self):
+        html_message = open(self.html_path).read()
+        html_message = html_message.replace('{logo_path}', self.logo_path).replace('{reservation_id}', str(self.dataID))
+
+        self.message = MIMEMultipart()
+        htmlMessageBody = MIMEText(html_message, 'html')
+
+        self.message.attach(htmlMessageBody)
+        with open('../../templates/HTML_EMAIL/images/Minutka_symulacjaRadia.pdf', "rb") as file:
+            attachment = MIMEApplication(file.read(), _subtype='pdf')
+        attachment.add_header('Content-Disposition', 'attachment', filename='Minutka_symulacjaRadia')
+        self.message.attach(attachment)
+
+        self.message['Subject'] = f'Your invoice for reservation {self.dataID} landed.'
+        self.message['From'] = EMAIL
+        self.message['To'] = ', '.join(self.recipients)
+        return self.message
+
+class CreatePaymentConfirmationMessage(CreateMail):
+    def __init__(self, dataID):
+        super().__init__(dataID)
+        self.html_path = '../../templates/HTML_EMAIL/payment.html'
+
+    def create_message(self):
+        html_message = open(self.html_path).read()
+        html_message = html_message.replace('{logo_path}', self.logo_path).replace('{reservation_id}', str(self.dataID))
+
+        self.message = MIMEText(html_message, 'html')
+        self.message['Subject'] = f'Payment for reservation {self.dataID} accepted!'
+        self.message['From'] = EMAIL
+        self.message['To'] = ', '.join(self.recipients)
+        return self.message
+
+class CreateActivationMessage(CreateMail):
+    def __init__(self, dataID):
+        super().__init__(dataID)
+        self.html_path = '../../templates/HTML_EMAIL/account-confirmation.html'
+
+    def create_message(self):
+        html_message = open(self.html_path).read()
+        html_message = html_message.replace('{logo_path}', self.logo_path).replace('{url_path}', 'onet.pl')
+
+        self.message = MIMEText(html_message, 'html')
+        self.message['Subject'] = f'Activate your account'
+        self.message['From'] = EMAIL
+        self.message['To'] = ', '.join(self.recipients)
+        return self.message
+class Mailing:
 
 
-SERVICE_ACCOUNT_FILE = 'google_credentials.json'
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+    def send_email(self, message, recipients):
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+           smtp_server.login(EMAIL, PASSWORD)
+           smtp_server.sendmail(EMAIL, recipients, message.as_string())
+        print("Message sent!")
 
-# Funkcja do uzyskania uwierzytelnienia za pomocą pliku klucza usługi
-def get_credentials():
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        SERVICE_ACCOUNT_FILE, SCOPES)
-    return credentials
-
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
-
-@app.route('/login')
-def login():
-    credentials = get_credentials()
-    session['credentials'] = credentials.to_json()
-    return redirect(url_for('authorized'))
-
-@app.route('/logout')
-def logout():
-    session.pop('credentials', None)
-    return redirect(url_for('index'))
-
-@app.route('/authorized')
-def authorized():
-    credentials_json = session.get('credentials')
-    if credentials_json:
-        credentials = ServiceAccountCredentials.from_json(json.loads(credentials_json))
-        # Tutaj możesz użyć credentials do wysłania wiadomości e-mail
-        return 'Logged in!'
-    else:
-        return 'Access denied!'
-
-
-@app.route('/send_mail/<string:mail>?<string:subject>', methods = ['POST'])
-def send_mail(mail:str, subject:str):
-    print(1)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+mailingObj = Mailing()
+messageObj = CreateActivationMessage(100)
+message = messageObj.create_message()
+mailingObj.send_email(message, messageObj.recipients)
