86569f1ce93082f49d19df274ee32ae0751c7dfb
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-03-27 17:33:55 +0100

    base test cases

--- test/test_config.py
+++ test/test_config.py
@@ -1,42 +1,36 @@
-from flask import current_app
-from flask_testing import TestCase
-
 from unittest import main
 
-from manage import app
-from src.config import config_by_name
+from src import create_app
+from test import BaseTestCase
 
 
-class TestDevelopmentConfig(TestCase):
+class TestDevelopmentConfig(BaseTestCase):
     def create_app(self):
-        app.config.from_object(config_by_name['dev'])
-        return app
+        self.app = create_app('dev')
+        return self.app
 
     def test_app_is_development(self):
-        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
-        self.assertTrue(app.config['SECRET_KEY'] is 'my_precious_secret_key')
-        self.assertTrue(app.config['DEBUG'] is True)
-        self.assertFalse(current_app is None)
+        self.assertFalse(self.app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(self.app.config['SECRET_KEY'] == 'my_precious_secret_key')
+        self.assertTrue(self.app.config['DEBUG'] is True)
+        self.assertFalse(self.app is None)
 
 
-class TestTestingConfig(TestCase):
-    def create_app(self):
-        app.config.from_object(config_by_name['test'])
-        return app
+class TestTestingConfig(BaseTestCase):
 
     def test_app_is_testing(self):
-        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
-        self.assertTrue(app.config['SECRET_KEY'] is 'my_precious_secret_key')
-        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertFalse(self.app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(self.app.config['SECRET_KEY'] == 'my_precious_secret_key')
+        self.assertTrue(self.app.config['DEBUG'] is True)
 
 
-class TestProductionConfig(TestCase):
+class TestProductionConfig(BaseTestCase):
     def create_app(self):
-        app.config.from_object(config_by_name['prod'])
-        return app
+        self.app = create_app('prod')
+        return self.app
 
     def test_app_is_production(self):
-        self.assertTrue(app.config['DEBUG'] is False)
+        self.assertTrue(self.app.config['DEBUG'] is False)
 
 
 if __name__ == '__main__':
