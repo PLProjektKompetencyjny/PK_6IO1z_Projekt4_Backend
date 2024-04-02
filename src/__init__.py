e345ffaf9f19a3d7582f7eef0f769f65fc2e2f11
rukaSUKII <sobonukasz@gmail.com>
2024-04-02 21:43:18 +0200

    Fix of Blueprint() statement location

--- src/__init__.py
+++ src/__init__.py
@@ -5,8 +5,8 @@ from logging.config import dictConfig
 from .config import config_by_name, LOGGING_CONFIG
 from .utils import db, flask_bcrypt
 from .controller.hello import hello
+from src.controller.mailing_controller import mailing
 
-mailing = Blueprint('mailing', __name__, url_prefix='/mailing')
 def create_app(config_name):
     app = Flask('TravelNest')
     app.config.from_object(config_by_name[config_name])
