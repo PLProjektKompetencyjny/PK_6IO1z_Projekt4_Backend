1b3d0ac1e56208a1813fd71d69bc5922751026bb
rukaSUKII <sobonukasz@gmail.com>
2024-04-02 21:44:52 +0200

    Add function for selecting message creator, HTTP codes implement for readability. Change the way endpoint take parameters from URL

--- src/controller/mailing_controller.py
+++ src/controller/mailing_controller.py
@@ -1,38 +1,48 @@
-from flask import Blueprint
 from smtplib import SMTPResponseException
+from flask import request, Blueprint
+from flask import current_app as app
+from http import HTTPStatus
 
 from src.service.mailing.mailservice import (CreateReservationConfirmationMessage, CreateInvoiceMessage,
                                              CreateResetPasswordMessage, CreatePaymentConfirmationMessage,
                                              CreateActivationMessage, MailService)
 
-from manage import app
-from src import mailing
+mailing = Blueprint('mailing', __name__, url_prefix='/mailing')
 
+def SelectMessageCreator(message_type, data_id, recipients):
+    if message_type == 1:
+        return CreateActivationMessage(data_id, recipients).create_message()
 
+    if message_type == 2:
+        return CreateResetPasswordMessage(data_id, recipients).create_message()
 
-@mailing.route('/sendmail/<int:data_id>/<string:address>/<int:message_type>', methods='POST')
-def sendmail(data_id, address, message_type):
+    if message_type == 3:
+      return CreateReservationConfirmationMessage(data_id, recipients).create_message()
+
+    if message_type == 4:
+      return CreateInvoiceMessage(data_id, recipients).create_message()
+
+    if message_type == 5:
+        return CreatePaymentConfirmationMessage(data_id, recipients).create_message()
+
+@mailing.route('/sendmail', methods=['POST'])
+def sendmail():
+    data_id = request.args.get('data_id', type=int)
+    address = request.args.get('address', type=str, default='')
+    message_type = request.args.get('message_type', type=int)
+
     if data_id < 0  or not address or message_type not in range(1,6):
         app.logger.warning(f'Invalid data passed to method')
-        return 400
+        return HTTPStatus.BAD_REQUEST.phrase, HTTPStatus.BAD_REQUEST
 
     recipients = address.split(',')
-    message = None
+
     try:
-        if message_type == 1:
-            message = CreateActivationMessage(data_id, recipients).create_message()
-        elif message_type == 2:
-            message = CreateResetPasswordMessage(data_id, recipients).create_message()
-        elif message_type == 3:
-            message = CreateReservationConfirmationMessage(data_id, recipients).create_message()
-        elif message_type == 4:
-            message = CreateInvoiceMessage(data_id, recipients).create_message()
-        elif message_type == 5:
-            message = CreatePaymentConfirmationMessage(data_id, recipients).create_message()
+        message = SelectMessageCreator(message_type, data_id, recipients)
     except ValueError as e:
         app.logger.error(f'''Problem occurred during message generation: {str(e)}.
                              passed data: data_id: {data_id}, address: {address}, message_type: {message_type}''')
-        return 500
+        return HTTPStatus.INTERNAL_SERVER_ERROR.phrase, HTTPStatus.INTERNAL_SERVER_ERROR
     finally:
         app.logger.info(f'Message created with id of data: {data_id} and type: {message_type}')
 
@@ -45,7 +55,7 @@ def sendmail(data_id, address, message_type):
         return 503
     except ValueError as e:
         app.logger.error(f'Value error: {str(e)}')
-        return 500
+        return HTTPStatus.INTERNAL_SERVER_ERROR.phrase, HTTPStatus.INTERNAL_SERVER_ERROR
     finally:
         app.logger.info(f'Mail to {recipients} with message type {message_type} has been sent successfully.')
-    return 200
+    return HTTPStatus.OK.phrase, HTTPStatus.OK
