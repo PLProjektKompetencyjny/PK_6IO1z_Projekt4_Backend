461dac76c88ed99828f76ecacccc71e5e60b7e72
rukaSUKII <sobonukasz@gmail.com>
2024-04-02 16:22:31 +0200

    Add blueprint of mailing controller

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 from logging.config import dictConfig
 
@@ -6,7 +6,7 @@ from .config import config_by_name, LOGGING_CONFIG
 from .utils import db, flask_bcrypt
 from .controller.hello import hello
 
-
+mailing = Blueprint('mailing', __name__, url_prefix='/mailing')
 def create_app(config_name):
     app = Flask('TravelNest')
     app.config.from_object(config_by_name[config_name])
@@ -15,6 +15,8 @@ def create_app(config_name):
     flask_bcrypt.init_app(app)
     db.init_app(app)
 
+
     app.register_blueprint(hello)
+    app.register_blueprint(mailing)
 
     return app
