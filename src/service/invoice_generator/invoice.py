ecab1ed12e97c631237b9731eec646acef1cab4c
rukaSUKII <sobonukasz@gmail.com>
2024-04-16 21:42:59 +0200

    Generator connected to DB - needs integration with flask connection.

--- src/service/invoice_generator/invoice.py
+++ src/service/invoice_generator/invoice.py
@@ -1,44 +1,104 @@
 import docxtpl
+from datetime import datetime
 
+from src.utils.utils import db
 from src.model.views.invoice_view import InvoiceView
 from src.model.views.reservation_view import ReservationView
 from src.model.views.customer_view import CustomerView
+from src.model.views.room_view import RoomView
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('postgresql://TN_admin:NestTravel@localhost/TravelNest')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class InvoiceGenerator:
-    def __init__(self, invoice_id):
-        self.invoice_details = InvoiceView.query.filter(invoice_id=invoice_id).one()
+    def __init__(self, some_id, tax):
+
+        self.invoice_view_details = session.query(
+                                        InvoiceView
+                                    ).filter(
+                                        InvoiceView.invoice_id == some_id
+                                    ).first()
+
+        self.customer_details = session.query(
+                                    CustomerView
+                                ).join(
+                                    ReservationView,
+                                    onclause=ReservationView.reservation_customer_id == CustomerView.customer_id
+                                ).filter(
+                                    ReservationView.reservation_id == self.invoice_view_details.invoice_reservation_id
+                                ).first()
+
+        self.reservation_details = (
+            session
+            .query(
+                ReservationView.reservation_id,
+                ReservationView.reservation_room_id,
+                ReservationView.reservation_number_of_adults,
+                ReservationView.reservation_number_of_children,
+                ReservationView.reservation_end_date - ReservationView.reservation_start_date,
+                (
+                        RoomView.room_gross_price +
+                        (RoomView.room_gross_price_adult * ReservationView.reservation_number_of_adults)+
+                        (RoomView.room_gross_price_child * ReservationView.reservation_number_of_children)
+                )
+            )
+            .join(
+                InvoiceView,
+                onclause=InvoiceView.invoice_reservation_id == ReservationView.reservation_id
+            )
+            .join(
+                RoomView,
+                onclause=RoomView.room_id == ReservationView.reservation_room_id
+            )
+            .filter(
+                InvoiceView.invoice_id == some_id
+            )
+            .all()
+                                    )
+        self.reservation_id = self.reservation_details[0][0]
+
+
+        tax = tax
+        tax_decimal = ((100-tax)/100)
+        self.rooms_details = []
+        gross_prices = []
+        net_prices =[]
+
+        for room in self.reservation_details:
+            gross_prices.append((room[4].days * room[5]))
+
+            net_prices.append(round(gross_prices[-1] * tax_decimal, 2))
+
+            self.rooms_details.append([room[1],
+                                       f'Adults: {room[2]}, children: {room[3]}',
+                                       room[4].days,
+                                       room[5],
+                                       net_prices[-1]]
+                                      )
 
         self.template_path = "../../templates/INVOICE/Invoice_template.docx"
-        self.customers_details = CustomerView.query.join(
-                                                        ReservationView, customer_id = ReservationView.reservation_customer_id).filter(
-                                                        ReservationView.reservation_id == self.invoice_details.reservation_id
-                                                        ).one()
 
-        invoice_list = [
-                        ['201', 'Room booking details: 1 child, 2 adults', 200, 200],
-                        ['202', 'Room booking details: 2 adults', 100, 100]
-                        ]
-
-        net_total = 0
-        for item in invoice_list: net_total += item[3]
-        tax = 8
-        self.data = {'id': invoice_id,
-                     'invoice_date': self.invoice_details.invoice_date,
-                     'name': f'{self.customers_details.customer_name} {self.customers_details.customer_surname}' ,
-                     'address': f'{self.customers_details.customer_street} {self.customers_details.customer_building_number}, {self.customers_details.customer_street} {self.customers_details.customer_postal_code}',
-                     'mail': self.customers_details.customer_email,
-                     'phone': self.customers_details.customer_phone.customer_phone,
+        self.data = {'id': some_id,
+                     'reservation_id': self.reservation_id,
+                     'invoice_date': self.invoice_view_details.invoice_date,
+                     'name': f'{self.customer_details.customer_name} {self.customer_details.customer_surname}' ,
+                     'address': f'{self.customer_details.customer_street} {self.customer_details.customer_building_number}, '
+                                f'{self.customer_details.customer_postal_code} {self.customer_details.customer_city}',
+                     'mail': self.customer_details.customer_email,
+                     'phone': self.customer_details.customer_phone,
                      'tax': f'{tax}%',
-                     'nip':self.customers_details.customer_nip_number,
-                     'net_total': net_total,
-                     'invoice_list': invoice_list,
-                     'total': ((100+8)/100) * net_total
+                     'nip':self.customer_details.customer_nip_number if self.customer_details.customer_nip_number is not None else '',
+                     'net_total': sum(net_prices),
+                     'invoice_list': self.rooms_details,
+                     'total': sum(gross_prices)
                      }
-
-
 
         self.invoice_file_obj = docxtpl.DocxTemplate(self.template_path)
         self.invoice_file_obj.render(self.data)
         self.invoice_file_obj.save('test.docx')
 
-
+InvoiceGenerator(1,8)
