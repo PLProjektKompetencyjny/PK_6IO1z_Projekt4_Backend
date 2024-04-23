5919fa2c841ce752bfec491578966f46ca33ac42
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-04-23 16:06:18 +0200

    add function to parse sqlalchemy exception

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,14 @@
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+def sqlalchemy_error_to_dict(e):
+    error_info = {
+        "type": type(e).__name__,
+        "message": str(e),
+        "code": getattr(e, 'code', 'Unknown')
+    }
+
+    return jsonify({"error": error_info})
