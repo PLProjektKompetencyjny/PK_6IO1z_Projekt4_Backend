61a56d200dec22d42f0f85f8ab7c785e0906d875
Stanisław Horna <stanislawhorna@outlook.com>
2024-05-27 16:43:18 +0200

    add DB function calculate_invoice_price

--- src/model/views/invoice_view.py
+++ src/model/views/invoice_view.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 
 from src.utils.utils import db
+from sqlalchemy import func
 
 
 @dataclass
@@ -21,14 +22,17 @@ class InvoiceView(db.Model):
 
     invoice_id = db.Column('invoice_id', db.Integer, primary_key=True)
     invoice_reservation_id = db.Column('invoice_reservation_id', db.Integer)
-    invoice_room_id = db.Column('invoice_room_id', db.Integer, primary_key=True)
+    invoice_room_id = db.Column(
+        'invoice_room_id', db.Integer, primary_key=True)
     invoice_room_price_gross = db.Column('invoice_room_price_gross', db.Float)
     invoice_date = db.Column('invoice_date', db.DateTime)
     invoice_price_gross = db.Column('invoice_price_gross', db.Float)
     invoice_is_paid = db.Column('invoice_is_paid', db.Boolean)
     invoice_status_id = db.Column('invoice_status_id', db.Integer)
-    invoice_last_modified_by = db.Column('invoice_last_modified_by', db.Integer)
-    invoice_last_modified_at = db.Column('invoice_last_modified_at', db.DateTime)
+    invoice_last_modified_by = db.Column(
+        'invoice_last_modified_by', db.Integer)
+    invoice_last_modified_at = db.Column(
+        'invoice_last_modified_at', db.DateTime)
 
     def __repr__(self):
         return (
@@ -43,3 +47,21 @@ class InvoiceView(db.Model):
             f'invoice_last_modified_by={self.invoice_last_modified_by}, '
             f'invoice_last_modified_at={self.invoice_last_modified_at})>'
         )
+    
+    @staticmethod
+    def calculate_invoice_price(reservation_id: int) -> None:
+        (
+            db
+            .session
+            .query(
+                func
+                .calculate_invoice_price(reservation_id)
+            )
+            .all()
+        )
+        (
+            db
+            .session
+            .commit()
+        )
+        return None
