29b7ca14ffce6e678fd63a5a61c998e8e632dd4a
Stanislaw Horna <stanislawhorna@outlook.com>
2024-05-27 22:24:37 +0200

    feature/DBfunctions (#39)
    * add services endpoint
    
    * return list of available services if no filters provided
    
    * simplify conversion
    
    * fix auto-formatting

--- src/controller/views/service_view_controller.py
+++ src/controller/views/service_view_controller.py
@@ -1,3 +1,4 @@
+from flask import request
 from abc import ABC
 from logging import getLogger
 
@@ -7,6 +8,9 @@ from src.controller.views.view_controller import ViewController
 
 class ServiceViewController(ViewController, ABC):
     def get(self):
+        if len(request.args.to_dict().items()) == 0:
+            return ServiceView.get_available_services(ServiceView, getLogger(__name__))
+        
         return self.get_rows(ServiceView, getLogger(__name__))
 
     def post(self):
--- src/model/views/service_view.py
+++ src/model/views/service_view.py
@@ -1,7 +1,16 @@
+from flask import request
 from dataclasses import dataclass
 from datetime import datetime
 
+from http import HTTPStatus
+from sqlalchemy.exc import SQLAlchemyError
+
 from src.utils.utils import db
+from sqlalchemy import func
+
+from src.controller.types.response import Response
+from src.controller.enums.database_response_status import DatabaseResponseStatus
+from src.utils.utils import sqlalchemy_error_to_dict
 
 
 @dataclass
@@ -34,3 +43,52 @@ class ServiceView(db.Model):
             f'service_last_modified_by={self.service_last_modified_by}, '
             f'service_last_modified_at={self.service_last_modified_at})>'
         )
+
+    @staticmethod
+    def get_available_services(model, logger) -> int:
+        filters = request.args.to_dict()
+        db_response = None
+
+        try:
+            db_output = (
+                db
+                .session
+                .query(
+                    func
+                    .get_available_services()
+                    .table_valued(
+                        'service_id',
+                        'service_name',
+                        'unit_price'
+                    )
+                )
+                .all()
+            )
+            db_response = []
+            for item in db_output:
+                db_response.append(
+                    ServiceView(
+                        **{
+                            "service_id": item[0],
+                            "service_name": item[1],
+                            "service_price": item[2]
+                        }
+                    )
+                )
+        except SQLAlchemyError as e:
+            json_data_error = sqlalchemy_error_to_dict(e)
+            logger.error(json_data_error)
+            return Response.create(DatabaseResponseStatus.DATABASE_ERROR.get_value(), [],
+                                   json_data_error.json), HTTPStatus.OK
+
+        row_count = len(db_response)
+
+        if not row_count:
+            logger.error(
+                f"No rows found in [{model.__tablename__}] with filters [{filters}]")
+            return Response.create(DatabaseResponseStatus.NOT_FOUND.get_value(), [],
+                                   DatabaseResponseStatus.NOT_FOUND.get_description()), HTTPStatus.OK
+
+        logger.info(f"Found [{row_count}] rows in [{model.__tablename__}] with filters [{filters}]")
+        return Response.create(DatabaseResponseStatus.OK.get_value(), db_response,
+                               DatabaseResponseStatus.OK.get_description()), HTTPStatus.OK
