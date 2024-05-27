22daa50907b90f39729b756f77087b9981425db0
Stanisław Horna <stanislawhorna@outlook.com>
2024-05-27 14:17:28 +0200

    adjust columns in mapping

--- src/model/views/invoice_view.py
+++ src/model/views/invoice_view.py
@@ -10,16 +10,22 @@ class InvoiceView(db.Model):
 
     invoice_id: int
     invoice_reservation_id: int
+    invoice_room_id: int
+    invoice_room_price_gross: float
     invoice_date: datetime
     invoice_price_gross: float
+    invoice_is_paid: bool
     invoice_status_id: int
     invoice_last_modified_by: int
     invoice_last_modified_at: datetime
 
     invoice_id = db.Column('invoice_id', db.Integer, primary_key=True)
     invoice_reservation_id = db.Column('invoice_reservation_id', db.Integer)
+    invoice_room_id = db.Column('invoice_room_id', db.Integer, primary_key=True)
+    invoice_room_price_gross = db.Column('invoice_room_price_gross', db.Float)
     invoice_date = db.Column('invoice_date', db.DateTime)
     invoice_price_gross = db.Column('invoice_price_gross', db.Float)
+    invoice_is_paid = db.Column('invoice_is_paid', db.Boolean)
     invoice_status_id = db.Column('invoice_status_id', db.Integer)
     invoice_last_modified_by = db.Column('invoice_last_modified_by', db.Integer)
     invoice_last_modified_at = db.Column('invoice_last_modified_at', db.DateTime)
@@ -28,7 +34,10 @@ class InvoiceView(db.Model):
         return (
             f'<InvoiceView(invoice_id={self.invoice_id}, '
             f'invoice_reservation_id={self.invoice_reservation_id}, '
+            f'invoice_room_id={self.invoice_room_id},'
+            f'invoice_room_price_gross={self.invoice_room_price_gross},'
             f'invoice_date={self.invoice_date}, '
+            f'invoice_is_paid={self.invoice_is_paid},'
             f'invoice_price_gross={self.invoice_price_gross},'
             f'invoice_status_id={self.invoice_status_id}, '
             f'invoice_last_modified_by={self.invoice_last_modified_by}, '
