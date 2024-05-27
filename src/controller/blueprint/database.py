92daeb407d850e1047dc5294532ad47cdb9cac60
Stanisław Horna <stanislawhorna@outlook.com>
2024-05-27 16:15:58 +0200

    add endpoint paths to new views

--- src/controller/blueprint/database.py
+++ src/controller/blueprint/database.py
@@ -5,6 +5,9 @@ from src.controller.views.room_view_controller import RoomViewController
 from src.controller.views.invoice_view_controller import InvoiceViewController
 from src.controller.views.user_view_controller import UserViewController
 from src.controller.views.reservation_view_controller import ReservationViewController
+from src.controller.views.room_type_mgmt_controller import RoomTypeMgmtController
+from src.controller.views.room_mgmt_controller import RoomMgmtController
+from src.controller.views.service_mgmt_controller import ServiceMgmtController
 
 database = Blueprint('database', __name__, url_prefix='/api')
 
@@ -17,3 +20,9 @@ database.add_url_rule('/invoices', view_func=InvoiceViewController.as_view('invo
 database.add_url_rule('/users', view_func=UserViewController.as_view('users'), methods=['GET'])
 
 database.add_url_rule('/reservations', view_func=ReservationViewController.as_view('reservations'), methods=['GET'])
+
+database.add_url_rule('/admin/room_type', view_func=RoomTypeMgmtController.as_view('room_type'), methods=['GET'])
+
+database.add_url_rule('/admin/room', view_func=RoomMgmtController.as_view('room'), methods=['GET'])
+
+database.add_url_rule('/admin/service', view_func=ServiceMgmtController.as_view('service'), methods=['GET'])
