fcbbb64e99b6b64479c93cfd576172dd4063a0d1
rukaSUKII <sobonukasz@gmail.com>
2024-06-25 18:09:32 +0200

    First steps for JWT implementation to endpoint

--- src/controller/blueprint/invoice.py
+++ src/controller/blueprint/invoice.py
@@ -1,11 +1,18 @@
+from logging import getLogger
+
+import jwt
+from logging import getLogger
 from flask import request, Blueprint, send_file, make_response
 from flask import current_app as app
 
 from http import HTTPStatus
 
 from src.model.types.invoice.InvoiceGenerator import InvoiceGenerator
+from src.model.views.reservation_view import ReservationView
 from src.service.invoice_generator.invoice import generate
 from src.controller.types.response import Response
+
+logger = getLogger(__name__)
 
 invoice = Blueprint('invoice_controller', __name__, url_prefix='/api')
 
@@ -13,6 +20,8 @@ def generate_invoice():
     if request.method != 'GET':
         app.logger.error('Incorrect request method. This endpoint only accepts GET')
         return Response.create(HTTPStatus.BAD_REQUEST, HTTPStatus.BAD_REQUEST.phrase, 'Incorrect method. Only GET is acceptable')
+
+
 
     reservation_id = request.args.get('reservation_id', type=int)
     tax_value = request.args.get('tax', type=int, default=8)
@@ -24,6 +33,11 @@ def generate_invoice():
     if reservation_id is None or reservation_id <=0:
         app.logger.error(f'Value of reservation id must be greater than 0. Value passed {reservation_id}')
         return Response.create(HTTPStatus.BAD_REQUEST,HTTPStatus.BAD_REQUEST.phrase,f'Value of reservation id must be greater than 0. Value passed {reservation_id}')
+
+    token = request.headers.get('Authorization').split("")[1]
+    data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+    client_id_reservation = ReservationView.get_customer_id_from_reservation_id(reservation_id, logger)
+
 
 
     invoice_generator_object = InvoiceGenerator(reservation_id, tax_value)
