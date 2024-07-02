bb0e12d17622d8dae17c75556045786267b12fdf
rukaSUKII <sobonukasz@gmail.com>
2024-07-02 21:34:34 +0200

    Change default value of invoice template

--- src/env.py
+++ src/env.py
@@ -11,4 +11,4 @@ JWT_SECRET_KEY = getenv('JWT_SECRET_KEY', 'tn_jwt_secret')
 SECRET_KEY = getenv('SECRET_KEY', 'my_precious_secret_key')
 
 INVOICE_DEST_PATH = getenv('INVOICE_DEST_PATH', default='/tmp')
-INVOICE_TEMPLATE_PATH = getenv('INVOICE_TEMPLATE_PATH', default='src/templates/INVOICE/Invoice_template.docx')
+INVOICE_TEMPLATE_PATH = getenv('INVOICE_TEMPLATE_PATH', default='src/model/templates/INVOICE/invoice_template.docx')
