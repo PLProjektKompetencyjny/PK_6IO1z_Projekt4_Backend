22081b830aa656671f136d5ab2e3cffa95f74b47
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-07-04 19:07:12 +0200

    room mgmt post and put

--- src/controller/blueprint/database.py
+++ src/controller/blueprint/database.py
@@ -30,6 +30,6 @@ database.add_url_rule('/services', view_func=ServiceViewController.as_view('serv
 database.add_url_rule('/admin/room_type', view_func=RoomTypeMgmtController.as_view('room_type'),
                       methods=['GET', 'POST', 'PUT'])
 
-database.add_url_rule('/admin/room', view_func=RoomMgmtController.as_view('room'), methods=['GET'])
+database.add_url_rule('/admin/room', view_func=RoomMgmtController.as_view('room'), methods=['GET', 'POST', 'PUT'])
 
 database.add_url_rule('/admin/service', view_func=ServiceMgmtController.as_view('service'), methods=['GET'])
